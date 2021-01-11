31f6d907ebe94d450f49cfe09de68a88bda0333c
ws <964487984@qq.com>
2021-01-11 15:39:23 +0800

    add rMain

--- main/java/com/ws/admin/controller/IndexController.java
+++ main/java/com/ws/admin/controller/IndexController.java
@@ -41,4 +41,9 @@ public class IndexController {
     public String reMain(){
         return "main";
     }
+
+    @GetMapping("/main.html")
+    public String rMain(){
+        return "main";
+    }
 }

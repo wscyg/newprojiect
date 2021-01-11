725018650b1e95354b53107537959f12835c2572
ws <964487984@qq.com>
2021-01-11 15:26:46 +0800

    init

---
+++ main/java/com/ws/admin/beans/User.java
@@ -0,0 +1,10 @@
+package com.ws.admin.beans;
+
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String Username;
+    private String password;
+}
---
+++ main/java/com/ws/admin/controller/IndexController.java
@@ -0,0 +1,44 @@
+package com.ws.admin.controller;
+
+
+import com.ws.admin.beans.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class IndexController {
+
+
+    /*
+    来登录页*/
+
+    @GetMapping(value={"/","/login"})
+    public String Loginpage(){
+         return "login";
+    }
+
+    //处理登录请求
+    @PostMapping("/login")
+    public String Main(User user, HttpSession session, Model model){
+        if(StringUtils.isEmpty(user.getUsername())&&StringUtils.isEmpty(user.getPassword())){
+            session.setAttribute("loginuser",user);
+            return "redirect:/main.html";
+        }else{
+            model.addAttribute("msg","账号密码错误");
+            return "login";
+        }
+        //转发return "main";
+        //重定向防止表单重复提交
+
+
+    }
+    @GetMapping("/main.html")
+    public String reMain(){
+        return "main";
+    }
+}
